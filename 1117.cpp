f17bbbe7442856e91692eb604263605c1c005cf5
yuyilei <yyl1352091742@gmail.com>
2017-04-18 19:37:08 +0800

    通过，存疑

--- 1117.cpp
+++ 1117.cpp
@@ -8,16 +8,15 @@ bool cmp1(int a, int b) {
 int main() {
     int n;
     scanf("%d", &n);
-        for(int i = 1; i <= n; i++) {
-                scanf("%d", &a[i]);
-                    }
-            sort(a+1, a+n+1, cmp1);
-                int ans = 0;
-                    int p = 1;
-                        while(ans <= n && a[p] > p) {
-                                ans++;
-                                p++;
-                            }
+    for(int i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+    sort(a+1, a+n+1, cmp1);
+    int ans = 0 , p = 1 ; 
+    while(ans <= n && a[p] > p) {
+        ans++;
+        p++;
+    }
     printf("%d", ans);
-        return 0;
+    return 0;
 }
