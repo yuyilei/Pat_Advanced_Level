64c9875b7cf680986bbcee243866e5c389c37f38
yuyilei <yyl1352091742@gmail.com>
2017-04-18 19:34:11 +0800

    通过,存疑

---
+++ 1117.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int a[1000000];
+bool cmp1(int a, int b) {
+    return a > b;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+        for(int i = 1; i <= n; i++) {
+                scanf("%d", &a[i]);
+                    }
+            sort(a+1, a+n+1, cmp1);
+                int ans = 0;
+                    int p = 1;
+                        while(ans <= n && a[p] > p) {
+                                ans++;
+                                p++;
+                            }
+    printf("%d", ans);
+        return 0;
+}
