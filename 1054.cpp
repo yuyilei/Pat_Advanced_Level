8f5914d82f20a35bc56fe92650ebc1720ef4ad63
yuyilei <yyl1352091742@gmail.com>
2017-04-19 14:20:47 +0800

    改编版, 通过

--- 1054.cpp
+++ 1054.cpp
@@ -1,10 +1,12 @@
 #include<cstdio>
 #include<iostream>
 #include<map>
-using namespace std ; 
+using namespace std ;
+//我知道这题有什么意义？？ ，而且我感觉用map 比建立 vector 合理！！！
 int main() {
     long m , n , i , j  , temp , max = 0 , out ;
     cin >> m >> n ;
+    max = (m*n) / 2 ; 
     map<long,long> a ; 
     for ( i = 0 ; i < n ; i++ ){
         for ( j = 0 ; j < m ; j++) {
@@ -20,10 +22,9 @@ int main() {
     for ( map <long,long> ::iterator it = a.begin() ; it != a.end() ; it++ ){
         int t = it->second ;  
         if ( t > max ){
-            max = t ;
             out = it->first ;
+            cout << out ; 
+            return 0 ; 
         }
     }
-    printf("%ld",out) ; 
-    return 0 ;
 }
