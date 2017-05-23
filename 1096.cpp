4983168dd4fb9add20ee93c892ea7e241f8f3d11
yuyilei <yyl1352091742@gmail.com>
2017-05-23 15:26:32 +0800

    通过

---
+++ 1096.cpp
@@ -0,0 +1,37 @@
+#include<cstdio>
+#include<math.h>
+using namespace std ; 
+int main() {
+    long  n , i , j , k ,  temp , a[200] , num = 0 , flag = 0 , tmp2  ; 
+    scanf("%ld",&n) ; 
+    float s = sqrt(n)  ; 
+    for ( i = 2 , j = 0 ; i <= s ; i++ ){
+        if ( n % i == 0 ){
+            a[j++] = i ; 
+        }
+    }
+    a[j++] = n ; 
+    for ( temp = 0 , num = 0 , k = j , i = 0 ; i < k ; i++ ){
+        temp = 0 ; 
+        tmp2 = n ; 
+        while ( true ){
+            if ( a[i+temp] == (a[i]+temp) && (tmp2 % a[i+temp] == 0 ) ) {
+                tmp2 /= a[i+temp] ; 
+                temp++ ; 
+            }
+            else {
+                break ; 
+            }       
+        }
+        if ( temp > num ){
+            num = temp ; 
+            flag = i ; 
+        }
+    }
+    printf("%ld\n",num) ; 
+    for ( i = flag ; i < flag + num - 1 ; i++ ){
+        printf("%ld*",a[i]) ; 
+    }
+    printf("%ld\n",a[flag+num-1]) ; 
+    return 0 ; 
+}
