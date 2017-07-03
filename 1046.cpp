a625848fdcd5565c5013960047fbd5186ffad422
yuyilei <yyl1352091742@gmail.com>
2017-07-04 00:08:07 +0800

    通过,略坑

--- 1046.cpp
+++ 1046.cpp
@@ -1,21 +1,20 @@
 #include<cstdio>
 using namespace std ; 
-int main() {
-    int i , j , n , m , *a , p , q , sum1 , sum2 , sum = 0 ; 
+int main() { // 无非就是比正向相加和反向相加的大小区别,sum是所有的数的总和,sum1是正向相加，sum2是反向相加 b[i] 是前i项的和
+    int i , j , n , m , *a , *b , p , q , sum1 , sum2 , sum = 0 ; 
     scanf("%d",&n) ; 
     a = new int[n] ; 
+    b = new int[n] ; 
     for ( i = 0 ; i < n ; i++ ){
         scanf("%d",&a[i]) ; 
         sum += a[i] ; 
+        b[i] = ( i == 0 ) ? ( a[0] ) : ( b[i-1] + a[i] ) ; 
     }
     scanf("%d",&m) ; 
     for ( i = 0 ; i < m ; i++ ){
         scanf("%d%d",&p,&q) ; 
-        p-- ; 
-        q-- ; 
         if ( p > q ) { j = q ; q = p ; p = j ; }
-        for ( sum1 = 0 , j = p ; j < q ; j++ )
-            sum1 += a[j] ; 
+        sum1 = b[q-2] - b[p-2] ;
         sum2 = sum - sum1 ; 
         printf("%d\n",((sum1 > sum2 ) ? sum2 : sum1) ) ; 
     }
