d57938e31e5a5f2928e153a2f6ac51f6170a6f08
yuyilei <yyl1352091742@gmail.com>
2017-07-03 23:58:14 +0800

    超时

--- 1046.cpp
+++ 1046.cpp
@@ -1,11 +1,13 @@
 #include<cstdio>
 using namespace std ; 
 int main() {
-    int i , j , n , m , *a , p , q , sum1 , sum2 ; 
+    int i , j , n , m , *a , p , q , sum1 , sum2 , sum = 0 ; 
     scanf("%d",&n) ; 
     a = new int[n] ; 
-    for ( i = 0 ; i < n ; i++ )
+    for ( i = 0 ; i < n ; i++ ){
         scanf("%d",&a[i]) ; 
+        sum += a[i] ; 
+    }
     scanf("%d",&m) ; 
     for ( i = 0 ; i < m ; i++ ){
         scanf("%d%d",&p,&q) ; 
@@ -14,15 +16,7 @@ int main() {
         if ( p > q ) { j = q ; q = p ; p = j ; }
         for ( sum1 = 0 , j = p ; j < q ; j++ )
             sum1 += a[j] ; 
-        for ( sum2 = 0 , j = p - 1 ; j >= 0 ; j--) { 
-            sum2 += a[j] ; 
-            if ( sum2 > sum1 ){
-                break ; 
-            }
-        }
-        for ( j = n - 1 ; j > q - 1 && sum2 <= sum1 ; j-- ) {
-            sum2 += a[j] ; 
-        }
+        sum2 = sum - sum1 ; 
         printf("%d\n",((sum1 > sum2 ) ? sum2 : sum1) ) ; 
     }
     return 0 ; 
