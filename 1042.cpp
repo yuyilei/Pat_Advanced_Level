b1e1ccbdbd32608bc055625f205d88ff9d20ca6c
yuyilei <yyl1352091742@gmail.com>
2017-07-04 23:53:21 +0800

    通过

---
+++ 1042.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std ; 
+int main() {
+    int a[55] , b[55] , i , j , num , res[55] , temp ; 
+    char out[5] = {'S' , 'H' , 'C' , 'D' , 'J' } ; 
+    cin >> num ; 
+    for ( i = 1; i < 55 ; i++ ){
+        cin >> a[i] ; 
+        b[i] = i ; 
+    }
+    for ( i = 0 ; i < num ; i++ ){
+        for ( j = 1 ; j < 55 ; j++ ){
+            res[a[j]] = b[j] ;
+        }      
+        for ( j = 1 ; j < 55 ; j++ ){
+            b[j] = res[j] ; 
+        }
+    }
+    for ( i = 1 ; i < 55 ; i++ ){
+        temp = ( res[i] - 1 ) / 13 ; 
+        cout << out[temp] << ( res[i] - temp * 13 ) ; 
+        if ( i != 54 ) cout << " " ; 
+    }
+    return 0 ; 
+}
