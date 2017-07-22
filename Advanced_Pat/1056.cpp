d9ead847c722528430a7b0c6d294aea83b62c87c
yuyilei <yyl1352091742@gmail.com>
2017-07-22 19:58:20 +0800

    -

--- Advanced_Pat/1056.cpp
+++ Advanced_Pat/1056.cpp
@@ -5,55 +5,43 @@ struct node {
     int weight , rank , flag ;
 } ;
 int main() {
-    int p , g , i , j , num = 0 , now = -1  , tmp  , max ;
+    int p , g , i , j , now = -1  , tmp , max  ;
     scanf("%d %d",&p,&g) ;
     struct node * ps =  new struct node  [p] ;
-    struct node maxn ;
-    int * pa = new int [p] ;
+    struct node maxn , t ;
     queue < struct node > qs ;
     for ( i = 0 ; i < p ; i++ ) {
         scanf("%d",&ps[i].weight) ;
-        ps[i].flag = 1 ;
+        ps[i].flag = i ;
     }
     for ( i = 0 ; i < p ; i++ ){
-         scanf("%d",&tmp) ;
-         pa[tmp] = i ;
+        scanf("%d",&tmp) ;
+        qs.push(ps[tmp])  ;  // 将老鼠按照顺序放入队列中
     }
-    for ( i = 0 ; i < p ; i++ )
-        qs.push(ps[pa[i]])  ;
     while ( qs.size() > 0  ) {
-        now = qs.size() / g + 2 ;
-        i = 0 ;
-        queue < struct node > temp ;
-        for ( j = 0 ; j < now - 1  ; j++ ) {
-            i = 0 ;
-            maxn = qs.front() ;
-            qs.pop() ;
-            while ( i < g - 1  ) {
-                struct node t = qs.front() ;
+        tmp = qs.size() ; // 本轮要比较的老鼠的数量
+        now = ( qs.size() % g  == 0 ) ? ( tmp / g +1 ) : ( tmp / g + 2 ) ;  // 本轮被淘汰的老鼠的名次
+        for ( j = 0 , max = -2  ; j < tmp  ; j++ ) {
+                t = qs.front() ;
                 qs.pop() ;
-                if ( maxn.weight < t.weight ) {
+                ps[t.flag].rank = now ;
+                if ( max < t.weight ) {
                     maxn = t ;
-                    maxn.rank = now ;
+                    max = t.weight ;
                 }
-                else {
-                    t.rank = now ;
+                if ( j == tmp - 1 ||  ( j + 1 ) % g == 0) {   // 本轮或本组比较结束, 将最大值放入队列里
+                    qs.push(maxn) ;
+                    max = -2 ;
                 }
-                if ( qs.size() == 0   )
-                    break ;
-                i++ ;
             }
-            temp.push(maxn) ;
-            printf("now1 %d \n",now) ;
-            printf("size1  %lu\n",temp.size() ) ;
-            printf("weight %d \n",maxn.weight) ;
+        if ( qs.size() == 1 ) {
+            t = qs.front() ;
+            ps[t.flag].rank = 1 ;
+            qs.pop() ;
         }
-        if ( temp.size() < g )
-            break ;
-        qs = temp ;
     }
-    for ( i = 0 ; i < g - 1 ; i++ )
+    for ( i = 0 ; i < p - 1 ; i++ )
         printf("%d ",ps[i].rank) ;
-    printf("%d\n",ps[g-1].rank) ;
+    printf("%d\n",ps[p-1].rank) ;
     return 0 ;
 }
