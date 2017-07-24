93519d14490d8db4e1cdc779628b3a2e35ad2e3a
yuyilei <yyl1352091742@gmail.com>
2017-07-24 19:51:29 +0800

    -

---
+++ Advanced_Pat/1067.cpp
@@ -0,0 +1,31 @@
+#include<cstdio>
+using namespace std ;
+void swap(int & a , int & b ) { int t = a ; a = b ; b = t ; }
+int main() {
+    int num , i , j , temp = 0 , tmp , flag = 0 , res = 0  ;
+    scanf("%d",&num) ;
+    int *p = new int [num] ;
+    for ( i = 0 ; i < num ; i++ ) {
+        scanf("%d",&tmp) ;
+        p[tmp] = i ;
+        if ( tmp != i ) temp++ ;
+    }
+    for ( i = 0 ; i < temp - 1  ; i++ ) {
+        if ( p[0] != 0 ) {
+            swap(p[0],p[p[0]]) ;
+        }
+        else {
+            for ( j = flag ; j < num ; j++ ) {
+                if ( p[j] != j ) {
+                    swap(p[0],p[j]) ;
+                    flag++ ;
+                    break ;
+                }
+            }
+            i-- ;
+        }
+        res++ ;
+    }
+    printf("%d\n",res) ;
+    return 0 ;
+}
