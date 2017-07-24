74031f8aebcaf0b58fe1565fd1b8daa79e838635
yuyilei <yyl1352091742@gmail.com>
2017-07-24 18:38:14 +0800

    -

--- Advanced_Pat/1067.cpp
+++ Advanced_Pat/1067.cpp
@@ -1,49 +1,59 @@
 #include<cstdio>
 #include<vector>
 #include<algorithm>
-using namespace std ; 
-int find_one(vector<int> a ,int  n , int key  ){
-    for ( int i = 0 ; i < n ; i++ ){
+using namespace std ;
+int find_one(vector<int> a , int key  ){
+    for ( int i = 0 ; i < a.size() ; i++ ){
         if ( a[i] == key ){
-            return i ; 
+            return i ;
         }
     }
 }
-int find_another(vector<int> a , int n ){
-    for ( int i = 1 ; i < n ; i++ ){
+int find_another(vector<int> a ){
+    for ( int i = 1 ; i < a.size() ; i++ ){
         if ( a[i] != i ) {
-            return i ; 
+            return i ;
         }
     }
-}
-void swap(vector<int> a  , int tmp1 , int tmp2 ){
-    int temp = a[tmp1] ; 
-    a[tmp1] = a[tmp2] ; 
-    a[tmp2] = temp ; 
+    return -1 ;
 }
 int main() {
-    int n , i ,j , temp = -1 , k , s ,  count = 0 , t = 0 ; 
-    scanf("%d",&n) ; 
-    vector<int> a (n) ; 
+    int n , i ,j , temp = 0 , k , s ,  count = 0 , t = 0 ;
+    scanf("%d",&n) ;
+    vector<int> a (n) ;
     for ( i = 0 ; i < n ; i++ ){
         scanf("%d",&a[i]) ;
         if ( a[i] != i ){
-            temp ++ ; 
+            temp ++ ;
         }
     }
-    while ( t <= temp ) {
-        k = find_one(a,n,0) ; 
+    if ( temp == 0 ) {
+        printf("0\n") ;
+        return 0 ;
+    }
+    int tmp = temp ;
+    while ( t < temp - 1  ) {
+        k = find_one(a,0) ;
         if  ( k == 0 ){
-            s = find_another(a,n) ; 
-            swap(a,k,s) ; 
-            count ++ ; 
-        } 
-        else {
-            s = find_one(a,n,k) ; 
-            swap(a,k,s) ; 
-            t++ ; 
+            s = find_another(a) ;
+        //    swap(a[k],a[s]) ;
+            printf("1\n" )  ;
+            count ++ ;
         }
+        else {
+            s = find_one(a,k) ;
+        //    swap(a[k],a[s]) ;
+            t++ ;
+            printf("2\n" )  ;
+        }
+        printf("swqp: %d %d\n",a[s],a[k]) ;
+     //   printf("t : %d  k : %d  s : %d \n",t,k,s) ;
+        int t = a[s] ;
+        a[s] = a[k] ;
+        a[k] = t ;
+      //  if ( k == 0 )
+           // break ;
     }
-    printf("%d\n",count+temp) ; 
-    return 0 ; 
+    printf("%d\n",count+tmp-1) ;
+    return 0 ;
 }
