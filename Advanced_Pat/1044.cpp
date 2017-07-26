c24093dddfff45ff299fa75408bd2754f56cfc52
yuyilei <yyl1352091742@gmail.com>
2017-07-27 02:06:03 +0800

    -

--- Advanced_Pat/1044.cpp
+++ Advanced_Pat/1044.cpp
@@ -1,44 +1,30 @@
 #include<cstdio>
-#define ALL 500
+#define ALL 100000
 using namespace std ;
 int main() {
-    int n , m , i , j , sum = 0 , min , flag = 0 , now = 0 , count = 0 , index = 0 ;
+    int n , m , i , j , min = 1000000000 , now = 0 , count = 0 ;
     scanf("%d %d",&n,&m) ;
     int **p = new int *[ALL] ;
     int *num = new int [n] ;
-    int a[ALL] ;
     for ( i = 0 ; i < ALL ; i++ ) { p[i] = new int [2] ; }
-    for ( i = 0 ; i < n   ; i++ ) {
+    for ( i = 0 ; i < n   ; i++ )
         scanf("%d",&num[i]) ;
-        sum += num[i] ;
-    }
-    min = sum ;
-    for ( i = 0 ; i < n ; i++ ) {
-        for ( j = i , now = 0 ; j < n ; j++ ) {
-            now += num[j] ;
-            if ( now >= m ) {
+    for ( i = 0 , j = 0 ; i < n && j <= n ;   ) {
+        if ( now < m ) { now += num[j++] ; }
+        else if ( now >= m ) {
+            if ( now <= min  ) {
                 if ( now < min ) {
                     count = 0 ;
                     min = now ;
-                    index = 0 ;
-                    a[count] = index++ ;
-                    p[a[count]][0] = i + 1 ;
-                    p[a[count++]][1] = j + 1 ;
-
-               }
-                else if ( now == min ) {
-                    a[count] = index++  ;
-                    p[a[count]][0] = i + 1 ;
-                    p[a[count++]][1] = j + 1 ;
-
                 }
-              //  printf("i %d , j %d , now %d\n",i,j,now) ;
-                break ;
+                p[count][0] = i + 1 ;
+                p[count++][1] = j  ;
             }
+            now -= num[i] ;
+            i++ ;
         }
     }
-    for ( i = 0 ; i < count ; i++ ) {
-        printf("%d-%d\n",p[a[i]][0],p[a[i]][1]) ;
-    }
+    for ( i = 0 ; i < count ; i++ )
+        printf("%d-%d\n",p[i][0],p[i][1]) ;
     return 0 ;
 }
